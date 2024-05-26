681a4b8077c05b7d4f9d8088d277607ca983d76e
Abhishek Soren <abhisheksoren95@gmail.com>
2024-05-26 22:04:50 +0530

    day26

---
+++ 3. Javascript - Basics to Advanced/103. JavaScript - Assignment Operators/script.js
@@ -0,0 +1,29 @@
+console.clear();
+
+var num1 = 15;
+var num2 = 30;
+
+// =
+num1 = 20;
+
+// +=
+num1 += 10; // num1 = num1 + 10
+num1 += num2; // num1 = num1 + num2
+console.log(num1);
+
+// -=
+num1 -= 10; //num1 = num1 - 10
+num1 -= num2; //num1 = num1 - num2
+console.log(num1);
+
+// *=
+num1 *= 20; //num1 = num1 * 20
+console.log(num1);
+
+// /=
+num1 /= 10; //num1 = num1 / 10
+console.log(num1);
+
+// %=
+num1 %= 10; //num1 = num1 % 10
+console.log(num1);

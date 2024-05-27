839a995b66f496d9f596d196613924be51630aff
Abhishek Soren <abhisheksoren95@gmail.com>
2024-05-27 15:37:19 +0530

    day27

---
+++ 3. Javascript - Basics to Advanced/105. JavaScript - Implicit Type Coercion/script.js
@@ -0,0 +1,27 @@
+console.clear();
+
+if ("Rocket" * 10) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+console.log(true + false);
+
+console.log(12 / "6");
+
+console.log("number" + 15 + 3);
+
+console.log(15 + 3 + "number");
+
+console.log(1 > null);
+
+console.log("foo" + +"bar");
+
+console.log("true" == true);
+
+console.log(false == "false");
+
+console.log(null == "");
+
+console.log(!!"false" == !!"true");

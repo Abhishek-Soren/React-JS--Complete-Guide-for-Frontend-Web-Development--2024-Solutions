7f64be08771579014121fd0d87e20159cfcfe1b5
Abhishek Soren <abhisheksoren95@gmail.com>
2024-06-03 14:50:02 +0530

    day30

--- 3. Javascript - Basics to Advanced/121. JavaScript - Update HTML Elements/script.js
+++ 3. Javascript - Basics to Advanced/121. JavaScript - Update HTML Elements/script.js
@@ -1,1 +1,18 @@
-document.getElementById("demo").innerHTML = "lorem ipsum";
+const element = document.getElementById("title");
+var bgImg = document.getElementById("bg");
+var i = 10;
+setInterval(function () {
+  if (i % 2 == 0) {
+    bgImg.style.backgroundImage = "linear-gradient(45deg, #845EC2, #FF6F91)";
+    console.group("even");
+  } else {
+    bgImg.style.backgroundImage = "linear-gradient(45deg, #051937, #A8EB12)";
+    console.group("odd");
+  }
+  element.innerHTML = i--;
+  console.log(i);
+
+  if (i < 0) {
+    element.innerHTML = 0;
+  }
+}, 1500);
